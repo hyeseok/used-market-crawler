78544ea4e4a3abeaaa7086c9ff6132ac9d1943fc
k.hyeseok <sukon1124@gmail.com>
2026-08-28 10:00:48 +0900

    chore: initialize Spring Boot project

---
+++ src/test/java/io/github/hyeseok/usedmarketcrawler/UsedMarketCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyeseok.usedmarketcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedMarketCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
